05f8020774a9fbdb397c0385eea0efcc90a31c66
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2024-04-26 01:17:35 -0500

    Axolotl no longer hunt Koi, because they are kind

--- src/main/java/com/teamabnormals/environmental/core/data/server/tags/EnvironmentalEntityTypeTagsProvider.java
+++ src/main/java/com/teamabnormals/environmental/core/data/server/tags/EnvironmentalEntityTypeTagsProvider.java
@@ -26,7 +26,6 @@ public class EnvironmentalEntityTypeTagsProvider extends EntityTypeTagsProvider 
 	public void addTags(Provider provider) {
 		this.tag(EntityTypeTags.IMPACT_PROJECTILES).add(MUD_BALL.get(), DUCK_EGG.get());
 		this.tag(EntityTypeTags.POWDER_SNOW_WALKABLE_MOBS).add(REINDEER.get());
-		this.tag(EntityTypeTags.AXOLOTL_HUNT_TARGETS).add(KOI.get());
 
 		this.tag(UNAFFECTED_BY_SERENITY);
 		this.tag(EnvironmentalEntityTypeTags.DEER).add(EnvironmentalEntityTypes.DEER.get(), REINDEER.get());
